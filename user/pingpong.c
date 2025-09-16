0eaba5a5b7029a37225a65d2a39f6ce22b52f0a6
AuroBreeze <1732373074@qq.com>
2025-09-16 12:03:24 +0800

    feat(user): 实现 pingpong 程序基本逻辑
    - 添加程序入口 main 函数
    - 实现命令行参数检查
    - 创建管道并进行进程 fork
    - 子进程关闭不必要的文件描述符并写入空格字符
    - 为后续的 ping 和 pong 功能实现打下基础

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,6 +3,23 @@
 #include "user/user.h"
 
 int main(int argc, char *argv[]){
-    
+    if(argc != 1){
+        fprintf(2, "Usage: pingpong\n");
+    }
+    int p[2];
+    pipe(p);
+    uint pid = fork();
+    uint parent_pid = getpid();
+
+    if(pid == 0){
+        close(0);
+        dup(p[0]);
+        close(p[0]);
+        close(p[1]);
+        write(1," ",1);
+    }else{
+        
+    }
+
     return 0;
 }
