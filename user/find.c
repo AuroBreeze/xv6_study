6592ea77f4d375a43f36e2974917e70c88f829b3
AuroBreeze <1732373074@qq.com>
2025-09-17 15:56:12 +0800

    feat(user): 添加 find 实用程序
    - 在 user 目录下新增 find.c 文件，实现文件查找功能
    - 更新 Makefile，将 _find 添加到 UPROGS 中
    - 实现了递归查找指定名称的文件，支持目录和普通文件的处理

--- user/find.c
+++ user/find.c
@@ -1,6 +1,74 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include "kernel/fs.h"
+#include "kernel/fcntl.h"
 
+static char *_basename(char *path){
+    char *p;
+
+    for(p = path+strlen(path); p>=path && *p != '/'; --p);
+    ++p;
+    return p;
+}
+int find(char *path, char *name){
+    char buf[512], *p;
+    int fd;
+    struct dirent de;
+    struct stat st;
+
+    if((fd = open(path, O_RDONLY)) < 0){
+        fprintf(2, "ls: cannot open %s\n", path);
+        return -1;
+    }
+
+    if(fstat(fd, &st) < 0){
+        fprintf(2, "ls: cannot stat %s\n", path);
+        close(fd);
+        return -1;
+    }
+    if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+        printf("find: path too long\n");
+        close(fd);
+        return -1;
+    }
+    strcpy(buf, path);
+    
+    short type = st.type;
+    if(type == T_FILE || type == T_DEVICE){
+        char *basename = _basename(path);
+        if(strcmp(name, basename) == 0){
+            fprintf(1, "%s\n", path);
+        }
+    }else if(type == T_DIR){
+        p = buf+strlen(buf);
+        *p++ = '/';
+        while(read(fd, &de, sizeof(de)) == sizeof(de)){
+            if(strcmp(de.name, ".") == 0 || strcmp(de.name, "..") == 0)
+                continue;
+            if(de.inum == 0)
+                continue;
+            memmove(p, de.name, DIRSIZ);
+            p[DIRSIZ] = 0;
+            if(stat(buf, &st) < 0){
+                fprintf(2, "find: connt stat %s\n", buf);
+                continue;
+            }
+            find(buf, name);
+        }
+    }
+    close(fd);
+    return 1;
+}
+
+int main(int argc, char *argv[]){
+    if(argc != 3){
+        fprintf(2, "Usage: find <path> <name>\n");
+        exit(0);
+    }
+    find(argv[1], argv[2]);
+    exit(0);
+    return 0;
+}
 
 
